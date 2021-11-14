6d37ec711d689595c91980a3b3ac70b6b46f2be1
Danil <GitRainDylan@users.noreply.github.com>
2021-11-14 17:50:19 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// incr;
+// decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(6%2);
+
+// console.log(2*4 === 8);
+
+const isCh = true,
+      isCl = 111;
+
+console.log(!isCl);
